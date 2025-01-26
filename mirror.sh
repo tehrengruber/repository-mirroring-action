4ce0d4a64115b2d780fa9af7577a1b4971147f31
Till Ehrengruber <till.ehrengruber@cscs.ch>
2025-01-26 20:29:15 +0100

    Fix mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -7,7 +7,7 @@ export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 # this started to fail as refs/remotes/origin/HEAD can not be pushed
 # $ git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
-git push -v --tags --force --prune origin_gitlab "refs/remotes/origin/*:refs/heads/*" ":refs/heads/HEAD"
+git push -v --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*" ":refs/heads/HEAD"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
